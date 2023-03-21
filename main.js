4889f77149bbdaae956f78bf345daae1a2ccb4c0
Christopher Saunders <christopherallansaunders@gmail.com>
2023-03-21 17:41:37 -0500

    Added comments gameplan for each file.

---
+++ main.js
@@ -0,0 +1,6 @@
+// Need use of media queries and classes to distinguish on page - hookup
+// Create functions to create user selection
+// create functions to generate computer selection
+// Calculate selection outcome of events
+// Calculate a formula to determine a winner
+// Use functions and forLoops where we can
