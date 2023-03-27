8e4511a036f3e971a3d458548bca764ab8b053ef
Christopher Saunders <christopherallansaunders@gmail.com>
2023-03-27 09:01:02 -0500

    Finsished product version 1.

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -1,18 +1,18 @@
 "use strict";
 
 // Need use of media queries and classes to distinguish on page - hookup
-var display_selection_com = document.querySelector("selection__com");
-var display_selection_user = document.querySelector("selection__user");
-var display_selection_result = document.querySelector("selection__result");
-var display_selection_choice = document.querySelectorAll("button");
+var display_selection_com = document.querySelector(".selection__com");
+var display_selection_user = document.querySelector(".selection__user");
+var display_selection_result = document.querySelector(".selection__result");
+var display_selection_choice = document.querySelectorAll(".selection__button");
 var selection__com;
 var selection__user;
 var selection__result; // Create functions to create user selection
 // Use functions and forLoops where we can
 
-display_selection_choice.forEach(function (possible__selection) {
-  return possible__selection.addEventListener("click", function (x) {
-    selection__user = x.target.id;
+display_selection_choice.forEach(function (selection__button) {
+  return selection__button.addEventListener("click", function (e) {
+    selection__user = e.currentTarget.id;
     display_selection_user.innerHTML = selection__user;
     generate_com_selection();
     generate__result();
@@ -21,53 +21,53 @@ display_selection_choice.forEach(function (possible__selection) {
 // Calculate selection outcome of events
 // Use 'if statements' to work through outcome of events
 
-function generate_com_selection() {
+var generate_com_selection = function generate_com_selection() {
   var random__number = Math.floor(Math.random() * 3) + 1;
 
   if (random__number === 1) {
-    selection__com = "_grass";
+    selection__com = "Grass";
   }
 
   if (random__number === 2) {
-    selection__com = "_fire";
+    selection__com = "Fire";
   }
 
   if (random__number === 3) {
-    selection__com = "_water";
+    selection__com = "Water";
   }
 
-  display_selection_choice.innerHTML = selection__com;
-} // Calculate a formula to determine a winner
+  display_selection_com.innerHTML = selection__com;
+}; // Calculate a formula to determine a winner
 
 
-function generate__result() {
+var generate__result = function generate__result() {
   if (selection__com === selection__user) {
     selection__result = "A Draw!";
   }
 
-  if (selection__com === "_grass" && selection__user === "_water") {
-    result = "You Win!";
+  if (selection__com === "Grass" && selection__user === "Water") {
+    selection__result = "You Lose!";
   }
 
-  if (selection__com === "_grass" && selection__user === "_fire") {
-    result = "You Lost!";
+  if (selection__com === "Grass" && selection__user === "Fire") {
+    selection__result = "You Lost!";
   }
 
-  if (selection__com === "_fire" && selection__user === "_water") {
-    result = "You Win!";
+  if (selection__com === "Fire" && selection__user === "Water") {
+    selection__result = "You Win!";
   }
 
-  if (selection__com === "_fire" && selection__user === "_grass") {
-    result = "You Lost!";
+  if (selection__com === "Fire" && selection__user === "Grass") {
+    selection__result = "You Lost!";
   }
 
-  if (selection__com === "_water" && selection__user === "_grass") {
-    result = "You Win!";
+  if (selection__com === "Water" && selection__user === "Grass") {
+    selection__result = "You Win!";
   }
 
-  if (selection__com === "_water" && selection__user === "_fire") {
-    result = "You Lost!";
+  if (selection__com === "Water" && selection__user === "Fire") {
+    selection__result = "You Lost!";
   }
 
   display_selection_result.innerHTML = selection__result;
-}
+};
--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // Need use of media queries and classes to distinguish on page - hookup
 
-const display_selection_com = document.querySelector("selection__com")
-const display_selection_user = document.querySelector("selection__user")
-const display_selection_result = document.querySelector("selection__result")
-const display_selection_choice = document.querySelectorAll("button")
+const display_selection_com = document.querySelector(".selection__com")
+const display_selection_user = document.querySelector(".selection__user")
+const display_selection_result = document.querySelector(".selection__result")
+const display_selection_choice = document.querySelectorAll(".selection__button")
 let selection__com
 let selection__user
 let selection__result
@@ -11,55 +11,55 @@ let selection__result
 // Create functions to create user selection
 // Use functions and forLoops where we can
 
-display_selection_choice.forEach(possible__selection => possible__selection.addEventListener("click", (x) => {
-    selection__user = x.target.id
+display_selection_choice.forEach(selection__button => selection__button.addEventListener("click", (e) => {
+    selection__user = e.currentTarget.id
     display_selection_user.innerHTML = selection__user
     generate_com_selection()
     generate__result()
-}))
+}));
 
 // create functions to generate computer selection
 // Calculate selection outcome of events
 // Use 'if statements' to work through outcome of events
 
-function generate_com_selection() {
+const generate_com_selection = () => {
     const random__number = Math.floor(Math.random() * 3) + 1
     
     if (random__number === 1) {
-        selection__com = "_grass"
+        selection__com = "Grass"
     }
     if (random__number === 2) {
-        selection__com = "_fire"
+        selection__com = "Fire"
     }
     if (random__number === 3) {
-        selection__com = "_water"
+        selection__com = "Water"
     }
-      display_selection_choice.innerHTML = selection__com
+      display_selection_com.innerHTML = selection__com
 }
 
 // Calculate a formula to determine a winner
 
-function generate__result() {
+const generate__result = () => {
     if (selection__com === selection__user) {
       selection__result = "A Draw!"
     }
-    if (selection__com === "_grass" && selection__user === "_water") {
-      result = "You Win!"
+    if (selection__com === "Grass" && selection__user === "Water") {
+      selection__result = "You Lose!"
     }
-    if (selection__com === "_grass" && selection__user === "_fire") {
-      result = "You Lost!"
+    if (selection__com === "Grass" && selection__user === "Fire") {
+      selection__result = "You Lost!"
     }
-    if (selection__com === "_fire" && selection__user === "_water") {
-      result = "You Win!"
+    if (selection__com === "Fire" && selection__user === "Water") {
+      selection__result = "You Win!"
     }
-    if (selection__com === "_fire" && selection__user === "_grass") {
-      result = "You Lost!"
+    if (selection__com === "Fire" && selection__user === "Grass") {
+      selection__result = "You Lost!"
     }
-    if (selection__com === "_water" && selection__user === "_grass") {
-      result = "You Win!"
+    if (selection__com === "Water" && selection__user === "Grass") {
+      selection__result = "You Win!"
     }
-    if (selection__com === "_water" && selection__user === "_fire") {
-      result = "You Lost!"
+    if (selection__com === "Water" && selection__user === "Fire") {
+      selection__result = "You Lost!"
     }
     display_selection_result.innerHTML = selection__result
   }
